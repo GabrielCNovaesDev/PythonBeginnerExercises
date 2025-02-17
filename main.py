c8555832f1eabf9c737fed44d8c18b0cf1bbb4c4
Gabriel Henrique <gabidelnovaes@gmail.com>
2025-02-17 19:57:29 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+#Exercício 1
+# itemQuantity = int(input("How many items would you like?"))
+# itemPrice = int(input("What is the price of the item?"))
+# FinalPrice = itemPrice*itemQuantity
+# print("The final price is", FinalPrice,"R$")
+import sys
+
+#Exercício 2
+# lugar = input("Digite o nome de um lugar:")
+# objeto = input("Digite o nome de um objeto:")
+# verbo = input("Digite o nome de um verbo:")
+#
+# print(f"hoje eu fui para o(a) {lugar}")
+# print(f"e logo após isso eu encontrei algo muito importante pra mim, um(a){objeto}")
+# print(f"por fim, eu peguei esse objeto e {verbo}")
+
+#Exercício 3 Calculadora
+# operator = int(input("Selecione a operação que deseja fazer! \n"
+#       "1 - (+)\n"
+#       "2 - (-)\n"
+#       "3 - (*)\n"
+#       "4 - (/)\n"))
+#
+# if operator > 4 or operator < 1:
+#     print("Escolha uma opção valida")
+#     sys.exit()
+#
+# value1 = float(input("Digite o primeiro valor: "))
+# value2 = float(input("Digite o segundo valor: "))
+#
+# if operator == 1:
+#     add = value1 + value2
+#     print(add)
+# elif operator == 2:
+#     subtract = value1 - value2
+#     print(subtract)
+# elif operator == 3:
+#     multiply = value1 * value2
+#     print(multiply)
+# elif operator == 4:
+#     divide = value1 / value2
+#     print(divide)
+
+# peso = float(input("digite seu peso"))
+# unidade = input("digite a unidade (K - kilo / L - libras)")
+#
+# if unidade == "K":
+#     peso *= 2.205
+#     unidade = "kgs"
+# elif unidade == "L":
+#     peso /= 2.205
+#     unidade = "lbs"
+# else:
+#     print("Opção inválida")
+#     sys.exit()
+#
+# print(f"O seu peso é de {round(peso, 2)} {unidade}")
+
+
+
+
